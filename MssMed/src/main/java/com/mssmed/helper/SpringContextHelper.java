984b65f530333806dfebd235dc5da367025e8e38
tkowalski <tkowalski@DITK.MSWS.LOCAL>
2014-02-24 14:22:22 +0100

    commit

---
+++ MssMed/src/main/java/com/mssmed/helper/SpringContextHelper.java
@@ -0,0 +1,27 @@
+package com.mssmed.helper;
+
+import com.vaadin.Application;
+import com.vaadin.terminal.gwt.server.WebApplicationContext;
+import javax.servlet.ServletContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class SpringContextHelper {
+
+    private ApplicationContext context;
+    public SpringContextHelper(ServletContext servletContext) {
+        /*ServletContext servletContext = 
+                ((WebApplicationContext) application.getContext())
+                .getHttpSession().getServletContext();*/
+        context = WebApplicationContextUtils.
+                getRequiredWebApplicationContext(servletContext);
+    }
+
+    public Object getBean(final String beanRef) {
+        return context.getBean(beanRef);
+    }    
+}
